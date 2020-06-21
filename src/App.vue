4ca2b9ff73db545b97b13c9b9330de61964e16e8
solidest <solidest@163.com>
2020-06-21 19:37:10 +0800

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,59 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <e-sys-bar />
+    <v-navigation-drawer permanent width="120">
+      <v-row class="fill-height" no-gutters>
+        <v-navigation-drawer mini-variant mini-variant-width="56" permanent>
+          <v-list-item class="px-2">
+            <v-list-item-avatar>
+              <v-img src="https://randomuser.me/api/portraits/women/75.jpg"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list dense nav>
+
+            <v-list-item v-for="item in items" :key="item.title">
+              <v-list-item-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+
+        <!-- <v-list class="grow">
+          <v-list-item v-for="link in links" :key="link" link>
+            <v-list-item-title v-text="link"></v-list-item-title>
+          </v-list-item>
+        </v-list> -->
+      </v-row>
+    </v-navigation-drawer>
+    <v-content>
+    </v-content>
+  </v-app>
 </template>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  ::-webkit-scrollbar {
+    display: none;
+  }
 </style>
+<script>
+  import ESysBar from './components/ESysBar'
+
+  export default {
+    name: 'App',
+
+    components: {
+      'e-sys-bar': ESysBar,
+    },
+
+    data: () => ({
+      //
+    }),
+  };
+</script>
---
+++ src/components/ESysBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-system-bar window dark id="app_bar">
+      <v-icon>mdi-message</v-icon>
+      <span>ETest Lite</span>
+      <v-spacer></v-spacer>
+      <v-icon class="nodrag">mdi-minus</v-icon>
+      <v-icon class="nodrag">mdi-checkbox-blank-outline</v-icon>
+      <v-icon class="nodrag">mdi-close</v-icon>
+    </v-system-bar>
+  </div>
+</template>
+<style>
+  #app_bar {
+    -webkit-app-region: drag;
+  }
+  .nodrag {
+    -webkit-app-region: no-drag;
+  }
+</style>
