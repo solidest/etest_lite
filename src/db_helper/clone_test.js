4673932152ca5442e50a7a30574d8c42fc0d6929
solidest <solidest@163.com>
2020-07-21 07:55:42 +0800

    udpate

--- src/db_helper/clone_test.js
+++
@@ -1,8 +0,0 @@
-
-import db from '../feature/m_db';
-import db_clone from './clone';
-import shortid from 'shortid';
-
-db.open('C:\\Users\\solidest\\AppData\\Roaming\\etest_dev')
-
-db_clone.get_clone_name()
