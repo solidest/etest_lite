354c62f67e0d0c2118ef2a8065d8a17deecc97c7
solidest <solidest@163.com>
2020-07-31 16:18:56 +0800

    update

--- src/components/panel/widgets_subpanel/EWidgetSwitch.vue
+++ src/components/panel/widgets_subpanel/EWidgetSwitch.vue
@@ -1,10 +1,20 @@
 <template>
-    <v-switch class="pa-0 ma-0 no-drag" color="white" :label="config.label" v-model="value" hide-details inset>
+    <v-switch class="pa-0 ma-0 no-drag" color="white" :label="config.label" v-model="value_" hide-details inset>
     </v-switch>
 </template>
 <script>
     import IWidget from './IWidget';
     export default {
         mixins: [IWidget],
+        computed: {
+            value_: {
+                set: function(v) {
+                    this.value = v ? 1 : 0;
+                },
+                get: function() {
+                    return this.value === 1;
+                }
+            }
+        }
     }
 </script>
