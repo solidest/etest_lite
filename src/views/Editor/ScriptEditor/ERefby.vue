47695fbd5ec1ab363cd5f324a3603e893630a0a9
solidest <solidest@163.com>
2020-11-09 10:35:43 +0800

    update

--- src/views/Editor/ScriptEditor/ERefby.vue
+++ src/views/Editor/ScriptEditor/ERefby.vue
@@ -1,9 +1,14 @@
 <template>
     <v-sheet class="d-flex flex-column" color="grey darken-3">
-        <v-card-subtitle class="py-3">执行用例 :</v-card-subtitle>
+        <v-card-subtitle class="py-1">
+            执行用例
+            <v-btn fab icon small color="white">
+                <v-icon small>mdi-plus-thick</v-icon>
+            </v-btn>
+        </v-card-subtitle>
         <v-divider />
         <div class="pa-2" :style="{height:`calc(100vh - ${top_height+48}px)`, width: '200px', 'overflow-y': 'auto'}">
-            <v-chip-group column active-class="primary--text">
+            <v-chip-group column>
                 <v-chip v-for="i in 30" :key="i" class="ma-1">
                     <v-avatar left>
                         <v-icon small>mdi-play</v-icon>
